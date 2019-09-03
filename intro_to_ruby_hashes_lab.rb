88f0a7aaa9801b3fda2f1fbb9e8e0981aab425fd
Peter Ayeni <ayenicology@gmail.com>
2019-09-03 09:40:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }
+      },
+      :name => {
+        
+      },
       :pieces => 4
     }
   }
