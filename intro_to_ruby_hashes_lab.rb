6074c90b2743ee4aa58b0040592d1106d0783bf6
Peter Ayeni <ayenicology@gmail.com>
2019-09-03 09:44:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,11 @@ def monopoly_with_third_tier
         },
         :pennsylvania_railroad =>{
         
-      },
+        },
+        :b_and_o_railroad =>{
+          
+        },
+      
       :pieces => 4
     }
   }
